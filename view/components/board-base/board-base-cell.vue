507778114266485d5db458e5cf093ade3782e5b9
joao melo <j@melo.plus>
2024-02-13 20:46:56 -0300

    feat: cell with borders

--- view/components/board-base/board-base-cell.vue
+++ view/components/board-base/board-base-cell.vue
@@ -2,12 +2,12 @@
 </script>
 <template>
   <div class="board-base-cell">
-    cell
+    X
   </div>
 </template>
 <style>
 .board-base-cell {
-  width: 20px;
-  height: 20px;
+  width: var(--size-55);
+  height: var(--size-55);
 }
 </style>
--- view/components/board-base/board-base.vue
+++ view/components/board-base/board-base.vue
@@ -4,19 +4,39 @@ import BoardBaseCell from "./board-base-cell.vue";
 <template>
   <div class="board-base">
     <board-base-cell />
+    <board-base-cell class="right left" />
     <board-base-cell />
+    <board-base-cell class="bottom top" />
+    <board-base-cell class="bottom top right left" />
+    <board-base-cell class="bottom top" />
     <board-base-cell />
-    <board-base-cell />
-    <board-base-cell />
-    <board-base-cell />
-    <board-base-cell />
-    <board-base-cell />
+    <board-base-cell class="right left" />
     <board-base-cell />
   </div>
 </template>
 <style>
 .board-base {
+  --border: solid var(--size-05) var(--color-black);
+}
+
+.board-base {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: max-content max-content max-content;
+}
+
+.right {
+  border-right: var(--border);
+}
+
+.left {
+  border-left: var(--border);
+}
+
+.top {
+  border-top: var(--border);
+}
+
+.bottom {
+  border-bottom: var(--border);
 }
 </style>
